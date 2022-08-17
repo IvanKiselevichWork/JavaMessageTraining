424ed809bb1c21ba7db868e4b8e547cc0c9730da
Kisialevich, Ivan [External] <Ivan.Kisialevich@externals.adidas.com>
2022-08-17 15:33:59 +0300

    Fix

--- 02_Active_MQ/active_mq_task/publisher/src/main/java/com/epam/publisher/service/publish/PublishServiceImpl.java
+++ 02_Active_MQ/active_mq_task/publisher/src/main/java/com/epam/publisher/service/publish/PublishServiceImpl.java
@@ -3,8 +3,6 @@ package com.epam.publisher.service.publish;
 import com.epam.publisher.model.Message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
