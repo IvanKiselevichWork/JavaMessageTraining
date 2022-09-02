763f29bfbb69a245bfde91809365f6374c3522a2
Kisialevich, Ivan [External] <Ivan.Kisialevich@externals.adidas.com>
2022-09-02 15:26:21 +0300

    Add dead letter queue

--- 03_Rabbit_MQ/rabbit_mq_task/consumer/src/main/java/com/epam/consumer/service/listen/ListenServiceImpl.java
+++ 03_Rabbit_MQ/rabbit_mq_task/consumer/src/main/java/com/epam/consumer/service/listen/ListenServiceImpl.java
@@ -3,7 +3,9 @@ package com.epam.consumer.service.listen;
 import com.epam.consumer.model.MyMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Argument;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
@@ -21,12 +23,32 @@ public class ListenServiceImpl {
 
     @RabbitListener(
             bindings = @QueueBinding(
-                    value = @Queue(name = "queue1"),
+                    value = @Queue(name = "deadQueue"),
+                    key = "rk.dead",
+                    exchange = @Exchange(name = "ex", type = ExchangeTypes.TOPIC)
+            )
+    )
+    public void listenDead(Message<MyMessage> message) {
+        log.info("Get message to dead queue: headers: {}, payload: {}", message.getHeaders(), message.getPayload());
+    }
+
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(
+                            name = "queue1",
+                            arguments = {
+                                    @Argument(name = "x-dead-letter-exchange", value = "ex"),
+                                    @Argument(name = "x-dead-letter-routing-key", value = "rk.dead"),
+                                    @Argument(name = "x-delivery-limit", value = "2")
+                            }),
                     key = "rk.1",
                     exchange = @Exchange(name = "ex", type = ExchangeTypes.TOPIC)
             )
     )
     public void listen1(Message<MyMessage> message) {
+        if (message.getPayload().getValue().equals("error")) {
+            throw new AmqpRejectAndDontRequeueException("Dead message");
+        }
         log.info("Get message to consumer 1: headers: {}, payload: {}", message.getHeaders(), message.getPayload());
     }
 
@@ -40,5 +62,4 @@ public class ListenServiceImpl {
     public void listen2(Message<MyMessage> message) {
         log.info("Get message to consumer 2: headers: {}, payload: {}", message.getHeaders(), message.getPayload());
     }
-
 }
